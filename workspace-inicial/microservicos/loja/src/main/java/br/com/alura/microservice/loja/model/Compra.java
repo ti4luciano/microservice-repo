0d380fdcd1995bc300135558dda95e3d5c2c71b3
Luciano Alves <ti4luciano@gmail.com>
2022-06-28 00:08:37 -0300

    Finnaly

--- workspace-inicial/microservicos/loja/src/main/java/br/com/alura/microservice/loja/model/Compra.java
+++ workspace-inicial/microservicos/loja/src/main/java/br/com/alura/microservice/loja/model/Compra.java
@@ -1,7 +1,20 @@
 package br.com.alura.microservice.loja.model;
 
-public class Compra {
+import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Compra {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private Long pedidoId;
@@ -9,14 +22,13 @@ public class Compra {
 	private Integer tempoDePreparo;
 	
 	private String enderecoDestino;
+	
+	private LocalDate dataParaEntrega;
+	
+	private Long voucher;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
+	@Enumerated(EnumType.STRING)
+	private CompraState state;
 
 	public Long getPedidoId() {
 		return pedidoId;
@@ -41,4 +53,38 @@ public class Compra {
 	public void setEnderecoDestino(String enderecoDestino) {
 		this.enderecoDestino = enderecoDestino;
 	}
+
+	public void setDataParaEntrega(LocalDate previsaoParaEntrega) {
+		this.dataParaEntrega = previsaoParaEntrega;
+	}
+
+	public LocalDate getDataParaEntrega() {
+		return dataParaEntrega;
+	}
+
+	public Long getVoucher() {
+		return voucher;
+	}
+
+	public void setVoucher(Long voucher) {
+		this.voucher = voucher;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setState(CompraState recebido) {
+		this.state = recebido;		
+	}
+	
+	public CompraState getState() {
+		return this.state;	
+	}
+	
+	
 }
